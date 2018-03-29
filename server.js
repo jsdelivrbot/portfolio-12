7bad88ff0f952c3bfe4e93f5a377709f9c562557
harryman <harrison.thomas04@gmail.com>
2018-03-29 14:15:33 -0600

    config json changes and server js changes for custom port per machine

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const logger = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
+
+// Get content from file
+var config = fs.readFileSync('config.json');
+// Define to JSON type
+var configJSON = JSON.parse(config);
+
+let port = configJSON.port;
 
 let app = express();
 
@@ -12,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-const port = 8080;
+//const port = 8080;
 
 app.listen(port, () => {
 	console.log(`Listening on http://localhost:${port}\n`);
