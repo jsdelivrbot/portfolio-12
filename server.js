e8e2f81df3652e423de3ab11dec066c3d85543ad
harryman <harrison.thomas04@gmail.com>
2018-03-29 13:29:28 -0600

    removed all the shit i don't need

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// controllers/routes
+const assignmentRouter = require('./routes/assignment.routes.js');
+
+// db
+// const db = require('./db');
+
+// load .env variables
+require('./config/config');
+
+let app = express();
+
+// middleware
+app.use(helmet());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const port = 80;
+
+app.listen(port, () => {
+	console.log(`Listening on http://adappt.tech:${port}\n`);
+});
+
+// db
+// 	.connect()
+// 	.then(() => {
+// 		console.log('\nconnection to database established\n');
+
+// 		app.listen(port, () => {
+// 			console.log(`Listening on http://adappt.tech:${port}\n`);
+// 		});
+// 	})
+// 	.catch(err => {
+// 		console.log('\nUnable to establish connection to the database\n');
+// 		console.log(err);
+// 		process.exit(1);
+// 	});
